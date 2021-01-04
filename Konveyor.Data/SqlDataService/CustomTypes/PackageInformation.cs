752151de2771745db7fb870290e82f0de0d471ae
Philip Newman <philipeano@gmail.com>
2021-01-04 18:22:52 +0100

    Extract custom helper types into separate files

---
+++ Konveyor.Data/SqlDataService/CustomTypes/PackageInformation.cs
@@ -0,0 +1,64 @@
+﻿using Konveyor.Core.Models;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Konveyor.Data.SqlDataService.CustomTypes
+{
+    public struct PackageInformation : IEquatable<PackageInformation>
+    {
+        public bool Equals([AllowNull] PackageInformation packageInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private Packages package;
+
+        private PackageUpdates initialUpdate;
+
+        private PackageUpdates recentUpdate;
+
+
+        public Packages Package
+        {
+            get
+            {
+                return package;
+            }
+
+            set
+            {
+                package = value;
+            }
+        }
+
+
+        public PackageUpdates InitialUpdate
+        {
+            get
+            {
+                return initialUpdate;
+            }
+
+            set
+            {
+                initialUpdate = value;
+            }
+        }
+
+
+        public PackageUpdates RecentUpdate
+        {
+            get
+            {
+                return recentUpdate;
+            }
+
+            set
+            {
+                recentUpdate = value;
+            }
+        }
+    }
+}
+
